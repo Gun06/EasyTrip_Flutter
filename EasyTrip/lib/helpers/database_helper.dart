38f2d7ebec37d691bca86cf5b3dcc71f8ba965a6
Gun06 <kogun0626@naver.com>
2024-07-04 00:23:13 +0900

    로컬 db구현

--- EasyTrip/lib/helpers/database_helper.dart
+++ EasyTrip/lib/helpers/database_helper.dart
@@ -4,28 +4,104 @@ import 'package:sqflite/sqflite.dart';
 import '../models/user.dart';
 
 class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper._internal();
-  factory DatabaseHelper() => instance;
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
   static Database? _database;
 
-  DatabaseHelper._internal();
+  DatabaseHelper._init();
 
   Future<Database> get database async {
     if (_database != null) return _database!;
-    _database = await _initDatabase();
+
+    _database = await _initDB('users.db');
     return _database!;
   }
 
-  Future<Database> _initDatabase() async {
-    String path = join(await getDatabasesPath(), 'user_database.db');
-    return openDatabase(
-      path,
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE users(id INTEGER PRIMARY KEY, password TEXT, name TEXT, nickname TEXT, birthDate TEXT, phoneNumber TEXT)',
-        );
-      },
-      version: 1,
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 2, onCreate: _createDB, onUpgrade: _upgradeDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      password TEXT NOT NULL,
+      name TEXT NOT NULL,
+      nickname TEXT NOT NULL,
+      birthDate TEXT NOT NULL,
+      phoneNumber TEXT NOT NULL,
+      profileImage TEXT,
+      isBlocked INTEGER NOT NULL DEFAULT 0
+    )
+    ''');
+  }
+
+  Future _upgradeDB(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 2) {
+      await db.execute('''
+      ALTER TABLE users ADD COLUMN profileImage TEXT;
+      ''');
+      await db.execute('''
+      ALTER TABLE users ADD COLUMN isBlocked INTEGER NOT NULL DEFAULT 0;
+      ''');
+    }
+  }
+
+  Future<void> blockUser(int userId) async {
+    final db = await database;
+    await db.update(
+      'users',
+      {'isBlocked': 1},
+      where: 'id = ?',
+      whereArgs: [userId],
+    );
+  }
+
+  Future<void> unblockUser(int userId) async {
+    final db = await database;
+    await db.update(
+      'users',
+      {'isBlocked': 0},
+      where: 'id = ?',
+      whereArgs: [userId],
+    );
+  }
+
+  Future<List<User>> getBlockedUsers() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'users',
+      where: 'isBlocked = ?',
+      whereArgs: [1],
+    );
+
+    return List.generate(maps.length, (i) {
+      return User.fromMap(maps[i]);
+    });
+  }
+
+  Future<List<User>> getUsers() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'users',
+      where: 'isBlocked = ?',
+      whereArgs: [0],
+    );
+
+    return List.generate(maps.length, (i) {
+      return User.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> deleteUser(int userId) async {
+    final db = await database;
+    await db.delete(
+      'users',
+      where: 'id = ?',
+      whereArgs: [userId],
     );
   }
 
@@ -38,27 +114,38 @@ class DatabaseHelper {
     );
   }
 
-  Future<List<User>> getUsers() async {
+  Future<void> updateUser(User user) async {
     final db = await database;
-    final List<Map<String, dynamic>> maps = await db.query('users');
-    return List.generate(maps.length, (i) {
-      return User(
-        id: maps[i]['id'],
-        password: maps[i]['password'],
-        name: maps[i]['name'],
-        nickname: maps[i]['nickname'],
-        birthDate: maps[i]['birthDate'],
-        phoneNumber: maps[i]['phoneNumber'],
-      );
-    });
+    await db.update(
+      'users',
+      user.toMap(),
+      where: 'id = ?',
+      whereArgs: [user.id],
+    );
   }
 
-  Future<void> deleteUser(int id) async {
+  Future<User?> getUser(int id) async {
     final db = await database;
-    await db.delete(
+    final maps = await db.query(
       'users',
+      columns: [
+        'id',
+        'password',
+        'name',
+        'nickname',
+        'birthDate',
+        'phoneNumber',
+        'profileImage',
+        'isBlocked'
+      ],
       where: 'id = ?',
       whereArgs: [id],
     );
+
+    if (maps.isNotEmpty) {
+      return User.fromMap(maps.first);
+    } else {
+      return null;
+    }
   }
 }
